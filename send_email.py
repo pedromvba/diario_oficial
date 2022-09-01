638fea77077ee6fb348b49fd3e263c50bd0a7b81
pedromvba <pedromvba@gmail.com>
2022-08-31 21:18:30 -0300

    Loading and Reading Environment Variables

--- send_email.py
+++ send_email.py
@@ -25,65 +25,41 @@ sender_email = os.getenv('EMAIL')
 password = os.getenv('PASSWORD')
 
 
+# Defining the function itself
+def send_email(subject, receiver_email, name, due_date, partnership, e_process):
+    # Creating the base message
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = formataddr(('Pedro Monteiro',f'{sender_email}'))  # shows the Pedro Monteiro as the sender instead
+                                                                    # of the email address
 
+    # E-mail body (plain text version)
+    msg.set_content(
 
+        f'''\
+        Saudações {name}, informo que a vigência da Parceria com {partnership}, processo {e_process} terá fim em {due_date}. 
+        Assim sendo, favor iniciar as tratativas internas para a renovação.     
+        '''
+    )
 
+    # Email body (html version)
 
+    msg.add_alternative(
 
+        f'''\
+    <html>
+    <body>
+        <p>Saudações {name}, informo que a vigência da Parceria com <strong>{partnership}</strong>, processo {e_process} terá fim em <strong>{due_date}</strong>.</p>
+        <p>Assim sendo, favor iniciar as tratativas internas para a renovação.</p>
+    </body>
+    </html>  
+        ''',
+        subtype = 'html'
+    )
 
+    with smtplib.SMTP_SSL(EMAIL_SERVER,PORT) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import smtplib
-# from email.message import EmailMessage
-#
-# msg = EmailMessage()
-#
-# my_address ="pedro.dataanalysis@gmail.com"    #sender address
-#
-# app_generated_password = "hpuxwfqqtxrxmsif"    # gmail generated password
-#
-# msg["Subject"] ="The Email Subject"   #email subject
-#
-# msg["From"]= my_address      #sender address
-#
-# msg["To"] = "pedromvba@gmail.com"     #reciver address
-#
-# msg.set_content("This is the body of the email")   #message body
-#
-# with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-#
-#     smtp.login(my_address,app_generated_password)    #login gmail account
-#
-#     print("sending mail")
-#     smtp.send_message(msg)   #send message
-#     print("mail has sent")
+if __name__ =='__main__':
+    send_email(subject='Due Date Reminder', name='Daniela', receiver_email='pedromvba@gmail.com', due_date = '01/09/2022', partnership='AMT', e_process='TLB-PRO-2022/487')
