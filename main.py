becc09c6e4359dafc57b10a59f1c04692eea0f0a
pedromvba <pedromvba@gmail.com>
2022-09-13 22:25:21 -0300

    Google Sheets Integration

--- main.py
+++ main.py
@@ -2,8 +2,27 @@
 Program to...
 '''
 
+from datetime import date
+from send_email import send_email
+
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
 
 
+# Connecting to Google Sheet
+
+service_account = gspread.service_account(filename='partnerships-due-dates-4ff6e6fd5095.json')
+sheet = service_account.open('Partnerships Due Dates')
+
+# Selecting the work sheet inside the file
+work_sheet = sheet.worksheet('Test Database')
+
+# Imports the data as a data frame
+df = pd.DataFrame(work_sheet.get_all_records())
+
+# Converts string to datetime type, coerce forces it even if the values are null or empty
+df['Data do Lembrete'] = pd.to_datetime(df['Data do Lembrete'], format='%d/%m/%Y', errors='coerce')
 
 
 
